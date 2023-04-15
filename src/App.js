92617608e0b915493f8ff570fa4dd2cd9c5b375d
seungm2n <seungm2n2@gmail.com>
2023-04-15 14:04:18 +0900

    코드 수정 및 모듈추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
+//import Header from './components/Header';
 import Footer from './components/Footer';
-// import Content from './components/Content';
 import Contact from './components/Contact';
 
 import './App.css';
@@ -11,8 +10,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header>
-      </Header>
         <Contact />
       <Footer>
       </Footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+//import Contact from './components/Contact';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Contact from './Contact';
+import App from '../App';
+
 
 class Header extends Component{
 	render() {
 	console.log('Header Component');
 		return(
-		    <header style={{background : "lightblue", border:"3px solid black"}}>
+		    <div style={{background : "lightblue", border:"3px solid black"}}>
 		        <div>LOGO IMG</div>
 		        <nav>
-		            <li>메뉴1</li>
-		            <li>메뉴2</li>
-		            <li>메뉴3</li>
-		        </nav>
-		    </header>
+		        <li><Link to="./" className="links">홈</Link></li>
+				<li><Link to="./contact" className="links"> 연락처 </Link></li>
+				</nav>
+				<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<App />}>연락처</Route>
+					<Route path="/contact" element={<Contact />}>연락처</Route>
+				</Routes>
+			</BrowserRouter>
+		    </div>
+		    
 		);
 	}
 }
+
 
 export default Header;
